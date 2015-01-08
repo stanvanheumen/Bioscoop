3052395221d3229c766e688d6a63247d2a6b8ef2
stanvanheumen <cjgheume@student.avans.nl>
2015-01-08 14:32:57 +0100

    Added unittesting project

---
+++ PROG5ASSESMENT/PROG5UNITTESTS/HomeControllerIndexTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PROG5ASSESMENT.Controllers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PROG5UNITTESTS
+{
+    [TestClass]
+    public class HomeControllerIndexTests
+    {
+        [TestMethod]
+        public void HomeIndexTests()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            //Assert.AreEqual("Hello, world!", result.ViewBag.Message);
+        }
+    }
+}
